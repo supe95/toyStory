cb9475fc2a6b64c9bde5f0c045b627b5695bb472
supeT540p <supeT540p@SUPET540>
2019-05-18 07:03:00 +0900

    prepare finders

--- 190518/src/main/java/org/toy/std/computer/Computer.java
+++ 190518/src/main/java/org/toy/std/computer/Computer.java
@@ -2,4 +2,5 @@ package org.toy.std.computer;
 
 public class Computer {
 
+	
 }
---
+++ 190518/src/main/java/org/toy/std/computer/finder/ScannerFinder.java
@@ -0,0 +1,16 @@
+package org.toy.std.computer.finder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.toy.std.computer.Scanner;
+
+public class ScannerFinder {
+
+	
+	public Set<Scanner> findScanner() {
+		Set<Scanner> scanners = new HashSet<Scanner>();
+		
+		return scanners;
+	}
+}
